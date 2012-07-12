dd38175e2951d85cf39863ed3942caaafb4369a1
Jake Lundberg <run.w.xcors@gmail.com>
2012-07-11 21:58:27 -0700

    Need new objects, references clobber for multi player

--- words.py
+++ words.py
@@ -35,41 +35,42 @@ initial_tiles = {'a': [ 9, 1 ],
                 'z': [ 1, 10 ],
                 '-': [ 2, 10 ]
                }
+def init_tiles():
+  initial_tiles = {'a': [ 9, 1 ],
+                   'b': [ 2, 4 ],
+                   'c': [ 2, 4 ],
+                   'd': [ 5, 2 ],
+                   'e': [ 13, 1 ],
+                   'f': [ 2, 4 ],
+                   'g': [ 3, 3 ],
+                   'h': [ 4, 3 ],
+                   'i': [ 8, 1 ],
+                   'j': [ 1, 10 ],
+                   'k': [ 1, 5 ],
+                   'l': [ 4, 2 ],
+                   'm': [ 2, 4 ],
+                   'n': [ 5, 2 ],
+                   'o': [ 8, 1 ],
+                   'p': [ 2, 4 ],
+                   'q': [ 1, 10 ],
+                   'r': [ 6, 1 ],
+                   's': [ 5, 1 ],
+                   't': [ 7, 1 ],
+                   'u': [ 4, 2 ],
+                   'v': [ 2, 5 ],
+                   'w': [ 2, 4 ],
+                   'x': [ 1, 8 ],
+                   'y': [ 2, 3 ],
+                   'z': [ 1, 10 ],
+                   '-': [ 2, 10 ]
+                  }
+  return initial_tiles
 
-#initial_tiles = {'a': {'count' : 9, 'score' : 1 },
-#                'b': {'count' : 2, 'score' : 4 },
-#                'c': {'count' : 2, 'score' : 4 },
-#                'd': {'count' : 5, 'score' : 2 },
-#                'e': {'count' : 13, 'score' : 1 },
-#                'f': {'count' : 2, 'score' : 4 },
-#                'g': {'count' : 3, 'score' : 3 },
-#                'h': {'count' : 4, 'score' : 3 },
-#                'i': {'count' : 8, 'score' : 1 },
-#                'j': {'count' : 1, 'score' : 10 },
-#                'k': {'count' : 1, 'score' : 5 },
-#                'l': {'count' : 4, 'score' : 2 },
-#                'm': {'count' : 2, 'score' : 4 },
-#                'n': {'count' : 5, 'score' : 2 },
-#                'o': {'count' : 8, 'score' : 1 },
-#                'p': {'count' : 2, 'score' : 4 },
-#                'q': {'count' : 1, 'score' : 10 },
-#                'r': {'count' : 6, 'score' : 1 },
-#                's': {'count' : 5, 'score' : 1 },
-#                't': {'count' : 7, 'score' : 1 },
-#                'u': {'count' : 4, 'score' : 2 },
-#                'v': {'count' : 2, 'score' : 5 },
-#                'w': {'count' : 2, 'score' : 4 },
-#                'x': {'count' : 1, 'score' : 8 },
-#                'y': {'count' : 2, 'score' : 3 },
-#                'z': {'count' : 1, 'score' : 10 },
-#                '-': {'count' : 2, 'score' : 10 }
-#               }
-               
 class wordsGame:
     
     def __init__(self, player):
       self.startTime = int(time.time())
-      self.tiles = initial_tiles 
+      self.tiles = init_tiles()
       self.op = player
       self.play_dict = {'me': { 'tiles' : [], 'score' : 0},
                         'op': { 'tiles' : [], 'score' : 0}}
@@ -114,3 +115,7 @@ class wordsGame:
 
       return outString
       
+
+
+
+#foo
