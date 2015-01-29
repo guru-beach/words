19806f61297ad41b15fdd7426b5e4b96b6a0a7a8
Jake Lundberg <jlundberg@adconion.com>
2015-01-29 15:33:09 -0800

    master: making easier to run on iphone

--- find_words.py
+++ find_words.py
@@ -1,28 +1,42 @@
 import sys
+import urllib2
 
+online_file = 'https://github.com/guru-beach/words/raw/master/word_list'
 infile = 'word_list'
-instring = sys.argv[1]
 max_length = 10
-try:
-  max_length = int(sys.argv[2])
-except:
-  pass
-
-#print sys.argv
-#print instring
-#print max_length
 
 def is_match(line, instring):
   for letter in instring:
     if letter not in line:
         return False
   return True
- 
+
+def download_words():
+  global infile
+  response = urllib2.urlopen(online_file)
+  with open(infile, 'w') as OF:
+    OF.write(response.read())
+  
+def check_file(infile):
+  try:
+    with open(infile, 'r') as IF:
+      pass
+  except IOError:
+    download_words()
+
 #print instring
-with open(infile, 'r') as wl:
-  for line in wl:
-   if is_match(line, instring):
-    if (len(line) <= max_length):
-      #print len(line) <= max_length
-      #print "{}: {}, {}".format(line, len(line), max_length)
-      print line.strip()
+def find_matches(instring, max_length):
+  check_file(infile)
+  with open(infile, 'r') as wl:
+   for line in wl:
+    if is_match(line, instring):
+     if (len(line) <= max_length):
+       print line.strip()
+
+if __name__ == '__main__':
+  instring = sys.argv[1]
+  try:
+    max_length = int(sys.argv[2])
+  except:
+    pass
+  find_matches(instring, max_length)
